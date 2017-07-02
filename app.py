12f0bfb81432179231a01845fea40c91686ef97c
Quinn Stearns <quinn@datahost.com>
2017-07-02 15:55:24 -0700

    added static file serving

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from emojis import emojis
 import random
-import yaml
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def serve():
+    return send_from_directory('static', 'index.html')
+
+@app.route('/static/<path:path>')
+def send_assets():
+    return send_from_directory('static', path)
 
 @app.route('/rand')
 def rand():
