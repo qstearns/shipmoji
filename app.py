fa2cb452ce776dece8236ead2f7df2e798a6ab5e
Quinn Stearns <quinn@datahost.com>
2017-07-02 12:10:20 -0700

    removed dumb stuff

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, url_for
 
 app = Flask(__name__)
 
-url_for('public', filename='styles.css')
-url_for('public', filename='index.html')
-
 @app.route('/')
 def hello_world():
     return 'Your mom'
